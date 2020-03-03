43f8bed09b3c5bb9fb0ec0e69fe44d7a6bab00e7
William Jackson <william@subtlecoolness.com>
2020-03-03 15:27:13 -0600

    Add some basic functionality

--- four_oh_four/app.py
+++ four_oh_four/app.py
@@ -1,2 +1,29 @@
+import flask
+import four_oh_four.settings
+import logging
+import sys
+import waitress
+import werkzeug.middleware.proxy_fix
+
+settings = four_oh_four.settings.Settings()
+
+app = flask.Flask(__name__)
+app.wsgi_app = werkzeug.middleware.proxy_fix.ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_port=1)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if flask.request.method == 'POST' and 'href' in flask.request.values:
+        href = flask.request.values.get('href')
+        app.logger.info(f'href: {href}')
+    return 'OK'
+
+
 def main():
-    pass
+    logging.basicConfig(format=settings.log_format, level='DEBUG', stream=sys.stdout)
+    app.logger.debug(f'four-oh-four {settings.version}')
+    if not settings.log_level == 'DEBUG':
+        app.logger.debug(f'Changing log level to {settings.log_level}')
+    logging.getLogger().setLevel(settings.log_level)
+
+    waitress.serve(app, ident=None, threads=settings.web_server_threads)
---
+++ four_oh_four/settings.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Settings:
+    def __init__(self):
+        self.log_format = os.getenv('LOG_FORMAT', '%(levelname)s [%(name)s] %(message)s')
+        self.log_level = os.getenv('LOG_LEVEL', 'INFO')
+        self.version = os.getenv('APP_VERSION', 'unknown')
+        self.web_server_threads = int(os.getenv('WEB_SERVER_THREADS', '4'))
